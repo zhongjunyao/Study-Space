97692c9d19b55819ed85cbaf2cc61059e92784b0
zjyCloud <15914468806@139.com>
2019-09-30 18:08:38 +0800

    nodeJS

--- nodejs/lesson2/router.js
+++ nodejs/lesson2/router.js
@@ -1,11 +1,11 @@
 
-function route(handle, pathname){
+function route(handle, pathname, response){
   console.log("About to route a request for " + pathname);
 
   if(typeof handle[pathname] === 'function'){
-    handle[pathname]();
+    handle[pathname](response);
   }else{
-    handle['/error']();
+    handle['/error'](response);
   }
 }
 
--- nodejs/lesson2/server.js
+++ nodejs/lesson2/server.js
@@ -6,17 +6,8 @@ function start (route, handle){
   function onRequest(request, response) {
     let pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-    
-    route(handle, pathname);
-
-    response.writeHead(200, {
-      "Content-Type": "text/plain"
-    });
-
-    response.write("Hello World !");
-    response.end();
+    route(handle, pathname, response);
   }
-
 
   http.createServer(onRequest).listen(8888);
 
